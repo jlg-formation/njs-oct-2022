39ec53b27264e1d0a0c801c8c5c1116db2247809
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-04 17:23:25 +0200

    dev: added ts

---
+++ back/src/server.ts
@@ -0,0 +1,23 @@
+(() => {
+  const express = require("express");
+  const serveIndex = require("serve-index");
+  const api = require("./api");
+
+  const app = express();
+  const port = 3000;
+  const wwwDir = "./public";
+
+  app.use((req, res, next) => {
+    console.log("req: ", req.url);
+    next();
+  });
+
+  app.use("/api", api);
+
+  app.use(express.static(wwwDir));
+  app.use(serveIndex(wwwDir, { icons: true }));
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
