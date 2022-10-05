7e1b32166f6bf3c12302c512e67b5c69fc0eb5a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-05 16:12:15 +0200

    dev: added asunchronous examples

---
+++ examples/async/promise.js
@@ -0,0 +1,9 @@
+const fs = require("node:fs/promises");
+
+fs.readdir(".")
+  .then((files) => {
+    console.log("files: ", files);
+    return fs.readFile(files[0], { encoding: "utf-8" });
+  })
+  .then((content) => console.log("content: ", content))
+  .catch((err) => console.log("err: ", err));
